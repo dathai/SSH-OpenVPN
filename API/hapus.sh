f459aa722dab6dc088abb49d9ba4d94272ed1fbe
dathai <googoogoo3@hotmail.com>
2017-11-11 09:25:54 +0700

    Update hapus.sh

--- API/hapus.sh
+++ API/hapus.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Script untuk menghapus user SSH & OpenVPN
 
-read -p "Nama user SSH yang akan dihapus : " Pengguna
+read -p "ชื่อผู้ใช้ SSH จะถูกลบออก : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna
-        echo -e "User $Pengguna telah dihapus."
+        echo -e "User $Pengguna ถูกลบแล้ว"
 else
-        echo -e "GAGAL: User $Pengguna tidak ada."
+        echo -e "GAGAL: User $Pengguna ลบไม่สำเร็จ"
 fi
